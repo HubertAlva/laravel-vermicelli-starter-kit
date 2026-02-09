f162800ec5ed62e30624d8ab3e520f5b1223d335
HubertAlva <hubertalvam@gmail.com>
2026-02-09 09:56:11 -0500

    vite.config fixed to use app domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,36 +2,40 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    server: {
-        host: '127.0.0.1',
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            host: 'vermicelli.local',
-            protocol: 'http',
-        },
-        cors: true,
-    },
-    plugins: [
-        laravel({
-            input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
-            refresh: true,
-        }),
-        tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        server: {
+            host: '127.0.0.1',
+            port: 5173,
+            strictPort: true,
+            hmr: {
+                host: env.VITE_APP_DOMAIN,
+                protocol: 'http',
             },
-        }),
-    ],
+            cors: true,
+        },
+        plugins: [
+            laravel({
+                input: ['resources/js/app.ts'],
+                ssr: 'resources/js/ssr.ts',
+                refresh: true,
+            }),
+            tailwindcss(),
+            wayfinder({
+                formVariants: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+    };
 });
