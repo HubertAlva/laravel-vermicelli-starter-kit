c184ce11919339f6064f359bb39575e4301320d1
HubertAlva <hubertalvam@gmail.com>
2026-03-22 10:10:30 -0500

    minor changes

--- resources/js/table/helpers/formatDate.ts
+++ resources/js/table/helpers/formatDate.ts
@@ -1,8 +1,10 @@
+import { dateLocale, dateTimeZone } from '@/lib/utils';
+
 export function formatDate(
     date: string,
     options?: Intl.DateTimeFormatOptions,
-    locale = import.meta.env.VITE_DATE_LOCALE,
-    timeZone = import.meta.env.VITE_DATE_TIMEZONE,
+    locale = dateLocale,
+    timeZone = dateTimeZone,
 ): string {
     return new Date(date).toLocaleDateString(locale, {
         timeZone,
